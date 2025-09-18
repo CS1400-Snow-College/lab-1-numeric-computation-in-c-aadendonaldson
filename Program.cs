ec3554d720ff3ec75803d9b64dbfe2285cd7d344
unknown <aaden.donaldson@students.snow.edu>
2025-09-18 17:21:03 -0600

    Twelth Commit

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-double a = 19;
-double b = 23;
-double c = 8;
-double d = (a + b) / c;
-Console.WriteLine(d);
+double max = double.MaxValue;
+double min = double.MinValue;
+Console.WriteLine($"The range of double is {min} to {max}");
