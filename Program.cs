b66f182a87f1304115e2b3d14480c587af733b96
unknown <aaden.donaldson@students.snow.edu>
2025-09-18 17:14:28 -0600

    eigth commit

--- Program.cs
+++ Program.cs
@@ -6,4 +6,6 @@ int d = (a + b) / c;
 int e = (a + b) % c;
 Console.WriteLine($"quotient: {d}");
 Console.WriteLine($"remainder: {e}");
-
+int max = int.MaxValue;
+int min = int.MinValue;
+Console.WriteLine($"The range of integers is {min} to {max}");
--- obj/Debug/net8.0/lab-1-numeric-computation-in-c-aadendonaldson.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab-1-numeric-computation-in-c-aadendonaldson.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-1-numeric-computation-in-c-aadendonaldson")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73fcfdca274a3342ecb4b85d02c3976f2d99348f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fdd52c74be903cd63fa20d43383e41fca1bef17")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-1-numeric-computation-in-c-aadendonaldson")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-1-numeric-computation-in-c-aadendonaldson")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
